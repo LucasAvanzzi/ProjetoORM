3f53de17e94faa525384cce94e12d1921422b0ea
curso.ads2 <curso.ads2@LAB6156>
2023-05-03 21:58:54 -0300

    Adicionar arquivos de projeto.

---
+++ Business/Cliente.cs
@@ -0,0 +1,22 @@
+﻿using DataBase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business
+{
+    public class Cliente : Base
+    {
+        [OpcoesBase(ChavePrimaria = true, UsaBD = true, UsaBusca = true)]
+        public int ID { get; set; }
+        [OpcoesBase(UsaBD = true)]
+        public string Nome { get; set; }
+        [OpcoesBase(UsaBD = true)]
+        public int Celular { get; set; }
+        [OpcoesBase(UsaBD = true)]
+        public  int Cpf { get; set; }
+
+    }
+}
---
+++ DataBase/IBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBase
+{
+    public interface IBase
+    {
+        void Salvar();
+        void Excluir();
+        int Key();
+        void CriarTabela();
+        List<IBase> Todos();
+        List<IBase> Buscar();
+    }
+}
+
+
